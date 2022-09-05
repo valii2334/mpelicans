7649f0ce55ce97406a16da3dd6a2d60445a045dc
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-05 22:38:55 +0300

    Add bootstrap theme AdminKit PRO

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,27 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// AdminKit (required)
+import "./modules/bootstrap";
+import "./modules/sidebar";
+import "./modules/theme";
+import "./modules/fullscreen";
+import "./modules/feather";
+
+// Global
+import "./modules/dragula";
+import "./modules/notyf";
+
+// Charts
+// import "./modules/apexcharts";
+// import "./modules/chartjs";
+
+// Forms
+import "./modules/choices";
+import "./modules/flatpickr";
+import "./modules/inputmask";
+import "./modules/quill";
+
+// Maps
+// import "./modules/vector-maps";
