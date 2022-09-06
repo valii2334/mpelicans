eae422fdbf7b68b112ced628e3f15e6e575e66e9
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-06 15:33:34 +0300

    Update smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
-    port:                 465,
+    port:                 587,
     domain:               'mpelicans.com',
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_API_KEY'],
