a82445bee2e2b0198d719d454c1a8543aef61d0c
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-06 21:53:02 +0300

    Update url host production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "mpelicans_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'mpelicans.com' }
+  config.action_mailer.default_url_options = { host: 'www.mpelicans.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
