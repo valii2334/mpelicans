3b1e94aaab5741779daf76f6be94c3a3969f5b5e
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-06 15:13:11 +0300

    Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.sendgrid.net',
+    port:                 465,
+    domain:               'mpelicans.com',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_API_KEY'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
