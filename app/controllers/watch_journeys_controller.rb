dc627cd48cd1ce774113c0b2d7258c5f294c363b
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-07 22:27:36 +0300

    Update forms

--- app/controllers/watch_journeys_controller.rb
+++ app/controllers/watch_journeys_controller.rb
@@ -3,7 +3,5 @@
 class WatchJourneysController < ApplicationController
   def show
     @journey = Journey.find_by(access_code: params[:access_code], access_type: :protected_journey)
-
-    render layout: 'simple'
   end
 end
