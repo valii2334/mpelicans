cde5b7452c6ded00576df281e871b5ddac0bdfa5
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-09-13 22:10:57 +0300

    Processed url for journey stop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def active_journey?(journey)
     return 'active' if params[:controller] == 'journeys' && params[:action] == 'show' && params[:id] == journey.id.to_s
+    return 'active' if params[:controller] == 'journey_stops' && params[:action] == 'new' && params[:journey_id] == String(journey.id)
   end
 
   def active_new_journey?
