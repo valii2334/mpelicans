07f83e1ae46425a54a400b2b7a5ccc0245b5ab10
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-10-11 10:36:50 +0300

    App cleanup

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,26 +2,11 @@
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
+import "./src/jquery"
+import "./src/jquery-ui"
+
 // AdminKit (required)
 import "./modules/bootstrap";
-import "./modules/sidebar";
 import "./modules/theme";
 import "./modules/fullscreen";
 import "./modules/feather";
-
-// Global
-import "./modules/dragula";
-import "./modules/notyf";
-
-// Charts
-// import "./modules/apexcharts";
-// import "./modules/chartjs";
-
-// Forms
-import "./modules/choices";
-import "./modules/flatpickr";
-import "./modules/inputmask";
-import "./modules/quill";
-
-// Maps
-// import "./modules/vector-maps";
