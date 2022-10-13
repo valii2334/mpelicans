9274376f22606394e62c7b5c33c6d39ff3d8c331
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-10-13 10:02:15 +0300

    Put sidebar functionality back

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,13 @@ import "./src/jquery-ui"
 
 // AdminKit (required)
 import "./modules/bootstrap";
+import "./modules/sidebar";
 import "./modules/theme";
 import "./modules/fullscreen";
 import "./modules/feather";
+
+$(document).ready(function () {
+  $('form').submit(function(){
+    $(this).find(':submit').attr('disabled','disabled');
+  });
+});
