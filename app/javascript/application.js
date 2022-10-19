d31178333b6d33a851e4787cf371fc932bae16ac
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-10-19 20:30:25 +0300

    Fix yarn error

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Entry point for the build script in your package.json
-import "./controllers";
+import "./controllers/application";
 import * as bootstrap from "bootstrap";
 
 import "./src/jquery";
