4af9cf274440e9d43604ceec2af14aefd3833d98
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2022-10-19 09:56:33 +0300

    Test User can create journey

--- cypress/e2e/journeys/create_journey.cy.js
+++ cypress/e2e/journeys/create_journey.cy.js
@@ -22,16 +22,20 @@ describe('User can create journey', function() {
       // Click on Start a New Journey
       cy.get('a').contains('Start a New Journey').click();
 
+      // Complete all journey info
       cy.get('#journey_title').fill('Going to Munchen');
       cy.get('#journey_description').fill('My first trip to Germany');
       cy.get('#journey_start_plus_code').fill('QJ24+HG Cluj-Napoca');
       cy.get('#journey_image').selectFile('./cypress/support/madrid.jpg');
 
+      // Submit the new journey
       cy.get('input').contains('Create').click();
 
-      // Check if we are on the journey page
+      // Check if we see all the info from above
       cy.contains('Going to Munchen');
       cy.contains('My first trip to Germany');
+      cy.get('#map-display').should('have.attr', 'src').should('include','QJ24%2BHG+Cluj-Napoca');
+      cy.get('#first-picture').should('have.attr', 'src').should('include','madrid.jpg');
 
       // Check if journey is selected in the sidebar
       cy.get('li.sidebar-item.active').contains('Going to Munchen');
