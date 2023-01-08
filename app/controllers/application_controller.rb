2b02bd9b0c57d61b8c4daa3791f332d47ff49f99
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-01-08 12:08:48 +0200

    Add toplevel documentation for ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# ApplicationController
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
