4b575a99498c1cec3f950b206a5ee631b1a21e70
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-01-08 12:49:31 +0200

    Update rspec tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :user do
     email    { FFaker::Internet.email }
     password { FFaker::Name.name }
-    username { FFaker::Name.name }
+    username { FFaker::Internet.user_name }
   end
 end
