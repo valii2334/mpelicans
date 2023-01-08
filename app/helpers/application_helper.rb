3318c6854ae5d72f13c5726e36cf0072ca94a16d
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-01-08 16:07:02 +0200

    Minor design updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,35 @@ module ApplicationHelper
   def journey_stop_belongs_to_journey?(journey)
     params[:journey_id] == String(journey.id)
   end
+
+  def journey_privacy_buttons(journey)
+    remaining_privacy_types(journey).map do |remaining_privacy_type|
+      journey_privacy_button(journey, remaining_privacy_type)
+    end
+  end
+
+  def journey_privacy_button(journey, privacy_type)
+    link_to "Make your journey #{privacy_type}",
+            journey_path(
+              journey,
+              journey: {
+                access_type: "#{privacy_type}_journey"
+              }
+            ),
+            method: :put,
+            confirm: 'Are you sure?',
+            class: 'btn btn-sm btn-outline-primary'
+  end
+
+  def remaining_privacy_types(journey)
+    if journey.private_journey?
+      %w[protected public monetized]
+    elsif journey.protected_journey?
+      %w[private public monetized]
+    elsif journey.public_journey?
+      %w[private protected monetized]
+    else
+      %w[private protected public]
+    end
+  end
 end
