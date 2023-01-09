9ab411dc3c9170ea237986fef2c1ddba4c040d61
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-01-09 17:49:12 +0200

    Update tests and rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,21 @@ class User < ApplicationRecord
   has_many :journeys, dependent: :destroy
   has_many :paid_journeys, dependent: :destroy
 
-  has_many :followed_users, foreign_key: :follower_id, class_name: 'Relationship'
-  has_many :followees, through: :followed_users, dependent: :delete_all
+  has_many :followed_users,
+           foreign_key: :follower_id,
+           class_name: 'Relationship',
+           dependent: :destroy,
+           inverse_of: :follower
 
-  has_many :following_users, foreign_key: :followee_id, class_name: 'Relationship'
-  has_many :followers, through: :following_users, dependent: :delete_all
+  has_many :followees, through: :followed_users
+
+  has_many :following_users,
+           foreign_key: :followee_id,
+           class_name: 'Relationship',
+           dependent: :destroy,
+           inverse_of: :followee
+
+  has_many :followers, through: :following_users
 
   validates :username, presence: true
   validates :username, uniqueness: true
