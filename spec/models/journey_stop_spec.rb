ab795d53ab42c72eb149a476ccb69094e61336fd
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-05-20 12:08:38 +0300

    test journey stop maximum number of images

--- spec/models/journey_stop_spec.rb
+++ spec/models/journey_stop_spec.rb
@@ -24,6 +24,19 @@ RSpec.describe JourneyStop, type: :model do
   it { should validate_presence_of :description }
   it { should validate_presence_of :plus_code }
 
+  describe '#maximum_number_of_images' do
+    it 'is not valid if number of images is gt MAXIMUM_NUMBER_OF_IMAGES' do
+      0.upto(JourneyStop::MAXIMUM_NUMBER_OF_IMAGES - 1).each do |_|
+        journey_stop.images << Rack::Test::UploadedFile.new('spec/fixtures/files/lasvegas.jpg', 'image/jpeg')
+      end
+
+      expect(subject).to_not be_valid
+      expect(subject.errors.messages).to eq(
+        { images: ["can't post more than #{JourneyStop::MAXIMUM_NUMBER_OF_IMAGES} images"] }
+      )
+    end
+  end
+
   ##################################
   # Associations
   ##################################
