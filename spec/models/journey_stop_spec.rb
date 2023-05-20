84b2c77fa87f90643dafdc6bfa82ad31a80c3b2b
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-05-20 12:17:34 +0300

    test passed_images bypass

--- spec/models/journey_stop_spec.rb
+++ spec/models/journey_stop_spec.rb
@@ -27,13 +27,26 @@ RSpec.describe JourneyStop, type: :model do
   describe '#maximum_number_of_images' do
     it 'is not valid if number of images is gt MAXIMUM_NUMBER_OF_IMAGES' do
       0.upto(JourneyStop::MAXIMUM_NUMBER_OF_IMAGES - 1).each do |_|
-        journey_stop.images << Rack::Test::UploadedFile.new('spec/fixtures/files/lasvegas.jpg', 'image/jpeg')
+        subject.images << Rack::Test::UploadedFile.new('spec/fixtures/files/lasvegas.jpg', 'image/jpeg')
       end
 
       expect(subject).to_not be_valid
       expect(subject.errors.messages).to eq(
         { images: ["can't post more than #{JourneyStop::MAXIMUM_NUMBER_OF_IMAGES} images"] }
       )
+    end
+  end
+
+  describe '#images_are_present' do
+    context 'passed_images is true' do
+      before do
+        subject.images = []
+        subject.passed_images = true
+      end
+
+      it 'is valid' do
+        expect(subject).to be_valid
+      end
     end
   end
 
@@ -48,10 +61,8 @@ RSpec.describe JourneyStop, type: :model do
   ##################################
 
   describe '#location_link' do
-    let(:journey_stop) { build(:journey_stop) }
-
     it 'returns location_link' do
-      expect(journey_stop.location_link).to eq("https://www.plus.codes/#{journey_stop.plus_code}")
+      expect(subject.location_link).to eq("https://www.plus.codes/#{subject.plus_code}")
     end
   end
 end
