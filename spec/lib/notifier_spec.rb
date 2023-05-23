7721cd75b1cb1292b53eac790a9beb591afbcf86
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-05-23 17:21:19 +0300

    minor fixes

--- spec/lib/notifier_spec.rb
+++ spec/lib/notifier_spec.rb
@@ -9,74 +9,13 @@ RSpec.describe Notifier do
   context 'public_journey' do
     let(:access_type) { :public_journey }
 
-    context '#bought_journey' do
-      include_context 'bought_journey set up'
+    it_behaves_like 'notifications are sent'
+  end
 
-      it 'creates one notification for the journey creator', aggregate: :failures do
-        expect do
-          subject
-        end.to change { Notification.count }.by(1)
+  context 'monetized_journey' do
+    let(:access_type) { :monetized_journey }
 
-        notification = Notification.last
-
-        expect(notification.sender).to eq(sender)
-        expect(notification.receiver).to eq(journey.user)
-        expect(notification.journey).to eq(journey)
-        expect(notification).to be_bought_journey
-      end
-    end
-
-    context '#new_journey' do
-      include_context 'new_journey set up'
-
-      it 'creates one notification for each follower', aggregate: :failures do
-        expect do
-          subject
-        end.to change { Notification.count }.by(2)
-
-        [user1, user2].each do |user|
-          notification = user.received_notifications.first
-
-          expect(notification.sender).to eq(journey.user)
-          expect(notification.receiver).to eq(user)
-          expect(notification.journey).to eq(journey)
-          expect(notification).to be_new_journey
-        end
-      end
-    end
-
-    context '#new_journey_stop' do
-      include_context 'new_journey_stop set up'
-
-      it 'creates one notification for each follower and paying journey user' do
-        expect do
-          subject
-        end.to change { Notification.count }.by(3)
-
-        [user1, user2, user3].each do |user|
-          notification = user.received_notifications.first
-
-          expect(notification.sender).to eq(journey.user)
-          expect(notification.receiver).to eq(user)
-          expect(notification.journey).to eq(journey)
-          expect(notification).to be_new_journey_stop
-        end
-      end
-    end
-
-    context '#random_notification_type' do
-      let(:notification_type) { :random_notification_type }
-
-      subject do
-        Notifier.new(journey_id: journey.id, notification_type:, sender_id: journey.user.id).notify
-      end
-
-      it 'raises an error' do
-        expect do
-          subject
-        end.to raise_error(StandardError)
-      end
-    end
+    it_behaves_like 'notifications are sent'
   end
 
   context 'other access type' do
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -79,3 +79,74 @@ RSpec.shared_context 'new_journey_stop set up' do
     Notifier.new(journey_id: journey.id, notification_type:, sender_id: journey.user.id).notify
   end
 end
+
+RSpec.shared_examples 'notifications are sent' do
+  context '#bought_journey' do
+    include_context 'bought_journey set up'
+
+    it 'creates one notification for the journey creator', aggregate: :failures do
+      expect do
+        subject
+      end.to change { Notification.count }.by(1)
+
+      notification = Notification.last
+
+      expect(notification.sender).to eq(sender)
+      expect(notification.receiver).to eq(journey.user)
+      expect(notification.journey).to eq(journey)
+      expect(notification).to be_bought_journey
+    end
+  end
+
+  context '#new_journey' do
+    include_context 'new_journey set up'
+
+    it 'creates one notification for each follower', aggregate: :failures do
+      expect do
+        subject
+      end.to change { Notification.count }.by(2)
+
+      [user1, user2].each do |user|
+        notification = user.received_notifications.first
+
+        expect(notification.sender).to eq(journey.user)
+        expect(notification.receiver).to eq(user)
+        expect(notification.journey).to eq(journey)
+        expect(notification).to be_new_journey
+      end
+    end
+  end
+
+  context '#new_journey_stop' do
+    include_context 'new_journey_stop set up'
+
+    it 'creates one notification for each follower and paying journey user' do
+      expect do
+        subject
+      end.to change { Notification.count }.by(3)
+
+      [user1, user2, user3].each do |user|
+        notification = user.received_notifications.first
+
+        expect(notification.sender).to eq(journey.user)
+        expect(notification.receiver).to eq(user)
+        expect(notification.journey).to eq(journey)
+        expect(notification).to be_new_journey_stop
+      end
+    end
+  end
+
+  context '#random_notification_type' do
+    let(:notification_type) { :random_notification_type }
+
+    subject do
+      Notifier.new(journey_id: journey.id, notification_type:, sender_id: journey.user.id).notify
+    end
+
+    it 'raises an error' do
+      expect do
+        subject
+      end.to raise_error(StandardError)
+    end
+  end
+end
