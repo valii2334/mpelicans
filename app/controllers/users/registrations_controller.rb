b18ca3bc1a1493c32ce9d4d4ab83e09f97917949
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-05-26 16:30:11 +0300

    fix tests

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -18,7 +18,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.save
     yield resource if block_given?
     if resource.persisted?
-      binding.pry
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
