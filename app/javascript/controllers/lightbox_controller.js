4cf2f7aa0ec797e64ff4e7c3b3dc19e617ab8354
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2023-07-28 17:06:31 +0300

    test lg lightbox

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   connect() {
     const options = {
       keyboard: true,
-      size: 'fullscreen',
+      size: 'lg',
       constraint: true
     };
     
