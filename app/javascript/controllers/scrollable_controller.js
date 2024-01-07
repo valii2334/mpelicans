cf990e72d46ad1172f2f4e10ca140bb37d99398d
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2024-01-07 19:34:23 +0200

    try new scrollable strategy 2

--- app/javascript/controllers/scrollable_controller.js
+++ app/javascript/controllers/scrollable_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   connect() {
     window.addEventListener('load', function () {
       const params = new URLSearchParams(window.location.search);
-      document.getElementById(params.get('scroll_to')).scrollIntoView(true);
 
       setTimeout(() => {
         document.getElementById(params.get('scroll_to')).scrollIntoView(true);
