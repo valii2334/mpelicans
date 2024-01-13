ccde1eb7c4ce7d16eca5faece3c26afb2ff189dc
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2024-01-13 17:53:02 +0200

    no need to call processed

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -63,7 +63,7 @@ module Imageable
   def image_urls(variant:)
     return [] unless processed?
 
-    images.map { |image| url_for(image.variant(variant).processed) }
+    images.map { |image| url_for(image.variant(variant)) }
   end
 
   def images_are_present
