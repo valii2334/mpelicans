95ddbd85f243fc9376353a8041011c7ae0d15aad
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2024-02-03 10:31:40 +0200

    compress html

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Mpelicans
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.middleware.use Rack::Deflater
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
