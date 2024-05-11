1bb1866da24865e46d05ab3c271502a06d67a3eb
Valentin Lazar <valentin.lazar.personal.email@gmail.com>
2024-05-11 05:46:08 +0300

    add link_to_google_maps to map_pin

--- app/models/map_pin.rb
+++ app/models/map_pin.rb
@@ -4,7 +4,8 @@ class MapPin < ApplicationRecord
   belongs_to :journey_stop, optional: true
   belongs_to :user
 
-  delegate :link_to_self, to: :journey_stop, allow_nil: true
+  delegate :link_to_self,        to: :journey_stop, allow_nil: true
+  delegate :link_to_google_maps, to: :journey_stop, allow_nil: true
 
   validates :lat, :long, :title, presence: true
   validates :journey_stop_id, uniqueness: { scope: :user_id }
